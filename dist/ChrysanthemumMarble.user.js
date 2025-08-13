e86e1f8e7b8a1ef4f52ccd26c1038f119deb1640
gori-GORILLA-gori <gorillasub777@gmail.com>
2025-08-13 16:36:24 +0900

    _

--- dist/ChrysanthemumMarble.user.js
+++ dist/ChrysanthemumMarble.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         菊のマーブル
 // @namespace    https://github.com/SwingTheVine/
-// @version      0.86.0
+// @version      0.87.0
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Chrysanthemum Marble" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
 // @author       DraftEditor
 // @license      MPL-2.0
 // @supportURL   https://discord.gg/tpeBPy46hf
 // @homepageURL  https://github.com/DraftEditor/ChrysanthemumMarble
 // @icon         https://raw.githubusercontent.com/DraftEditor/ChrysanthemumMarble/master/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com//gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com//gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
+// @updateURL    https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.js
 // @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM.setValue
 // @grant        GM_getValue
-// @resource     CSS-BM-File https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/master/dist/ChrysanthemumMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/gori-GORILLA-gori/ChrysanthemumMarble/refs/heads/main/dist/ChrysanthemumMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
